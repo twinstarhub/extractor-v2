a0f4ee3e9e8141c9e4b7fe25f2bb7cbebff49972
twinstarhub <golddev9697@gmail.com>
2023-06-30 09:31:14 +0200

    Update extractor

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ en_file_path_in = os.environ['INPUT_EN_FILE_PATH']
 cn_file_path_in = os.environ['INPUT_CN_FILE_PATH']
 en_file_path_out = os.environ['OUTPUT_EN_FILE_PATH']
 cn_file_path_out = os.environ['OUTPUT_CN_FILE_PATH']
+result_path_out = os.environ['RESULT_JSON_FILE_PATH']
 test_path = os.environ['OUTPUT_TEST_FILE_PATH']
 
 
 # This is a English to Chinese matching script
 if __name__ == '__main__':
-
-    tw_extractor = Extractor(en_file_path_in,cn_file_path_in,en_file_path_out,cn_file_path_out)
+    limit_score = input("Enter a limit score: ")
+    tw_extractor = Extractor(en_file_path_in,cn_file_path_in,en_file_path_out,cn_file_path_out,result_path_out,limit_score)
     tw_extractor.generate_paragraph()
-    print('This is matching script')
+    
 
